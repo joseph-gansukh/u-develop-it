6eec74506ea50bcbe3f95db76d2f3279071ed0b0
Joseph S Gansukh <g.sukhbat@gmail.com>
2021-02-06 00:04:19 -0600

    added parties table

--- server.js
+++ server.js
@@ -24,30 +24,30 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //   console.log(rows);
 // });
 
-db.get(`SELECT * FROM candidates WHERE id = 100`, (err, row) => {
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
   if (err) console.log(err)
   console.log(row)
 })
 
 // Delete a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this, this.changes);
-});
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
 
 // Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
               VALUES (?,?,?,?)`;
 const params = [1, 'Ronald', 'Firbank', 1];
 // ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this.lastID);
-});
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
 
 app.get("/", (req, res) => {
